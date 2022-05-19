854afc59fea2f94b8f3a4c47c6fec7a9656c4702
Víctor <victorlaurencena@gmail.com>
2022-05-19 20:02:44 -0300

    Product cart border only on hovering

--- src/components/ProductsListItem.js
+++ src/components/ProductsListItem.js
@@ -2,10 +2,31 @@ import React, { Component } from "react";
 import Link from "react-router-dom/Link";
 
 class ProductsListItem extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hover: false,
+        }
+        this.handleOnMouseEnter = this.handleOnMouseEnter.bind(this);
+        this.handleOnMouseLeave = this.handleOnMouseLeave.bind(this);
+    }
+
+    handleOnMouseEnter() {
+        this.setState({
+            hover: true
+        });
+    }
+
+    handleOnMouseLeave() {
+        this.setState({
+            hover: false
+        });
+    }
 
     render() {
         return (
-            <div className="box-shadow product-item-container">
+            <div onMouseEnter={this.handleOnMouseEnter} onMouseLeave={this.handleOnMouseLeave}
+                className={"product-item-container " + (this.state.hover ? "box-shadow" : "")}>
                 <Link to={`${this.props.category}/${this.props.id}`}>
                     <div className="product-item-container__image" style={{ backgroundImage: "url(" + this.props.image + ")" }}>
                         <span className="product-item-container__out-of-stock">{!this.props.inStock && "OUT OF STOCK"}</span>
